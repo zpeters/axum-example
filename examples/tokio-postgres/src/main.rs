75f49cf0641d4af26aa0d9c88397d4268ee142a7
AlexanderChen1989 <alexanderchenmh@gmail.com>
2021-10-16 23:12:20 +0800

    update

--- examples/tokio-postgres/src/main.rs
+++ examples/tokio-postgres/src/main.rs
@@ -1,50 +1,58 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-tokio-postgres
-//! ```
-
 use axum::{
     async_trait,
-    extract::{Extension, FromRequest, RequestParts},
-    handler::get,
+    extract::{Extension, FromRequest, RequestParts, Path},
+    handler::{get, post},
     http::StatusCode,
-    AddExtensionLayer, Router,
+    response::IntoResponse,
+    AddExtensionLayer, Json, Router,
 };
 use bb8::{Pool, PooledConnection};
 use bb8_postgres::PostgresConnectionManager;
-use std::net::SocketAddr;
+
+use std::{collections::HashMap, net::SocketAddr};
+use tokio::runtime::Builder;
 use tokio_postgres::NoTls;
 
-#[tokio::main]
-async fn main() {
-    // Set the RUST_LOG, if it hasn't been explicitly defined
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "example_tokio_postgres=debug")
-    }
-    tracing_subscriber::fmt::init();
+use serde::Serialize;
 
-    // setup connection pool
-    let manager =
-        PostgresConnectionManager::new_from_stringlike("host=localhost user=postgres", NoTls)
+fn main() {
+    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+
+    rt.block_on(async {
+        // Set the RUST_LOG, if it hasn't been explicitly defined
+        if std::env::var_os("RUST_LOG").is_none() {
+            std::env::set_var("RUST_LOG", "example_tokio_postgres=debug")
+        }
+
+        tracing_subscriber::fmt::init();
+
+        let conf = "host=localhost user=postgres password=postgrespassword dbname=postgres";
+
+        // setup connection pool
+        let manager = PostgresConnectionManager::new_from_stringlike(conf, NoTls).unwrap();
+        let pool = Pool::builder().build(manager).await.unwrap();
+
+        // build our application with some routes
+        let app = Router::new()
+            .route("/", post(using_connection_extractor))
+            .route("/:id", get(using_connection_pool_extractor))
+            .layer(AddExtensionLayer::new(pool));
+
+        // run it with hyper
+        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        tracing::debug!("listening on {}", addr);
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await
             .unwrap();
-    let pool = Pool::builder().build(manager).await.unwrap();
+    });
+}
 
-    // build our application with some routes
-    let app = Router::new()
-        .route(
-            "/",
-            get(using_connection_pool_extractor).post(using_connection_extractor),
-        )
-        .layer(AddExtensionLayer::new(pool));
-
-    // run it with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+#[derive(Debug, Serialize)]
+struct User {
+    id: i32,
+    name: String,
+    age: i32,
 }
 
 type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
@@ -52,21 +60,20 @@ type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 // we can exact the connection pool with `Extension`
 async fn using_connection_pool_extractor(
     Extension(pool): Extension<ConnectionPool>,
-) -> Result<String, (StatusCode, String)> {
-    let conn = pool.get().await.map_err(internal_error)?;
+    Path(parts): Path<HashMap<String, String>>,
+) -> Result<(StatusCode, impl IntoResponse), (StatusCode, String)> {
+    let id = parts.get("id").unwrap();
+    let conn = pool.get_owned().await.map_err(internal_error)?;
 
-    let row = conn
-        .query_one("select 1 + 1", &[])
-        .await
-        .map_err(internal_error)?;
-    let two: i32 = row.try_get(0).map_err(internal_error)?;
+    let user = get_user_witd_id(&conn, id.clone()).await?;
 
-    Ok(two.to_string())
+    Ok((StatusCode::FOUND, Json(user)))
 }
 
 // we can also write a custom extractor that grabs a connection from the pool
 // which setup is appropriate depends on your application
-struct DatabaseConnection(PooledConnection<'static, PostgresConnectionManager<NoTls>>);
+type Conn = PooledConnection<'static, PostgresConnectionManager<NoTls>>;
+struct DatabaseConnection(Conn);
 
 #[async_trait]
 impl<B> FromRequest<B> for DatabaseConnection
@@ -87,22 +94,44 @@ where
 }
 
 async fn using_connection_extractor(
-    DatabaseConnection(conn): DatabaseConnection,
-) -> Result<String, (StatusCode, String)> {
+    DatabaseConnection(conn): DatabaseConnection
+) -> Result<(StatusCode, Json<User>), (StatusCode, String)> {
+    let user = get_user(&conn).await?;
+    Ok((StatusCode::FOUND, Json(user)))
+}
+
+async fn get_user(conn: &Conn) -> Result<User, (StatusCode, String)> {
     let row = conn
-        .query_one("select 1 + 1", &[])
+        .query_one("select * from users limit 1", &[])
         .await
         .map_err(internal_error)?;
-    let two: i32 = row.try_get(0).map_err(internal_error)?;
 
-    Ok(two.to_string())
+    let id: i32 = row.try_get("id").map_err(internal_error)?;
+    let name: String = row.try_get("name").map_err(internal_error)?;
+    let age: i32 = row.try_get("age").map_err(internal_error)?;
+
+    Ok(User { id, name, age })
+}
+
+async fn get_user_witd_id(conn: &Conn, id: String) -> Result<User, (StatusCode, String)> {
+    let query = format!("select * from users where id={} limit 1", id);
+    
+    let row = conn
+        .query_one(query.as_str().clone(), &[])
+        .await
+        .map_err(internal_error)?;
+
+    let id: i32 = row.try_get("id").map_err(internal_error)?;
+    let name: String = row.try_get("name").map_err(internal_error)?;
+    let age: i32 = row.try_get("age").map_err(internal_error)?;
+
+    Ok(User { id, name, age })
 }
 
 /// Utility function for mapping any error into a `500 Internal Server Error`
 /// response.
 fn internal_error<E>(err: E) -> (StatusCode, String)
 where
-    E: std::error::Error,
-{
+    E: std::error::Error, {
     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
